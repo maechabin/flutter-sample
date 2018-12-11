a9f069aed04feadfc3c320ec9ed31a74ba22a336
maechabin <mitoppo@gmail.com>
2018-12-12 01:42:06 +0900

    コメント翻訳wip

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,20 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // このウィジェットはアプリケーションおルートです。
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
+        // これはアプリケーションのテーマです。
         //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        // "flutter run"でアプリケーションを起動してみましょう。青いツールバーを持ったアプリが開きます。
+        // それから、アプリを終了することなく、下記のprimarySwatchをColors.greenに変更してみると、
+        // "hot reload"が呼び出されます（"flutter run"を起動したコンソールで"r"を押すか、
+        // Flutter IDE内の"hot reload"に対して変更を保存するかだけでもOKです）。
+        // カウンターはゼロにリセットされていないということに注目してください。
+        // アプリケーションは再起動されていません。
         primarySwatch: Colors.red,
       ),
       home: MyHomePage(title: 'Flutter Sample'),
