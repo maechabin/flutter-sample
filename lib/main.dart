8edd872f74cc99360d245c673d3c5a0978aa406f
maechabin <mitoppo@gmail.com>
2018-12-12 22:30:43 +0900

    コメントを翻訳

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // このウィジェットはアプリケーションおルートです。
+  // このウィジェットはアプリケーションのルートです。
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,15 +27,13 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  // このウィジェットはアプリケーションのホームページです。ステートフルであり、
+  // （以下で定義されている）Stateオブジェクトを持っていることを意味しています。
+  // このStateオブジェクトには、見た目に影響を及ぼすフィールドを含んでいます。
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  // このクラスはstateのための設定を行います。親によって提供され（今回のケースではtitle）、
+  // Stateのbuildメソッドによって使われる（今回のケースではApp widget）値を保持します。
+  // Widgetサブクラスのフィールドは常に"final"でマークされます。
   final String title;
 
   @override
@@ -47,11 +45,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+      // このsetStateの呼び出しは、FlutterフレームワークにこのStateの中で何かが変更されたということを伝えます。
+      // これにより、以下のbuildメソッドがreturnされ、更新された値が反映されます。
+      // もし、setState()を呼び出さずに、_counterを変更したとしたら、
+      // buildメソッドは再び呼ばれることはなく、何も起こらないでしょう。
       _counter++;
     });
   }
@@ -60,34 +57,31 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
+    // このメソッドはsetStateが呼ばれるたびにreturnされます。
+    // たとえば、上記の_incrementCounterメソッドによってreturnされるように。
     //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // Flutterフレームワークはbuildメソッドが速くreturnするように最適化してきました。
+    // そのためWidgetのインスタンスを個別に変更する必要はなく、更新が必要なものだけを再buildできます。
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        // ここではApp.buildメソッドによって作成されたMyHomePageオブジェクトから値を取得し、
+        // appbarのtitleを設定するために使用します。
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+        // Centerはレイアウト用のwidgetです。単一の子を取り、親の中央に配置します。
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
+          // Columnもまたレイアウト用のwidgetです。子のリストを取り、それらを垂直に並べます。
+          // デフォルトでは、子のサイズに合わせてサイズが変更され、親の高さになります。
           //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
+          // 各ウィジェットのワイヤフレームを表示するために"debug painting"を起動します。
+          //（コンソールで"p"を押すか、
+          // Android StudioであればFlutterインスペクターから"Toggle Debug Paint"アクションを選択するか、
+          // Visual Studio Codeであれば"Toggle Debug Paint"コマンドを実行します。）
           //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+          // Columnは、自分自身の大きさや子の配置方法を制御するためのさまざまなプロパティを持ちます。
+          // ここでは、子を垂直方向にセンタリングするためにmainAxisAlignmentを使用しています。
+          // ここでの主軸は垂直軸となります。それは列が垂直（交差軸は水平）であるからです。
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -104,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), // この末尾のカンマは、buildメソッドのための自動フォーマットをより良くします。
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,10 @@
-// This is a basic Flutter widget test.
+// これは基本的なwidgetのテストです。
 //
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+// テストでウィジェットとのインタラクションを実行するために、
+// Flutterが提供するWidgetTesterユーティリティを使用します。
+// たとえば、タップとスクロールのジェスチャを送信することなどができます。
+// さらに、WidgetTesterを使用して、widgetのツリー内の子widgetを検索し、
+// テキストを読み取り、widgetのプロパティの値が正しいことを検証することもできます。
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -12,18 +13,18 @@ import 'package:flutter_sample/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+    // アプリをbuildしてフレームをトリガーします。
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
+    // カウンターが0からスタートするか検証します。
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
+    // '+'アイコンをタップし、フレームをトリガーします。
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    // Verify that our counter has incremented.
+    // カウンターが増えるか検証します。
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
