aabf88d7bc1262767389244bef3f479244c86dfd
maechabin <mitoppo@gmail.com>
2018-12-18 22:04:33 +0900

    Hello World実装

--- lib/main.dart
+++ lib/main.dart
@@ -2,101 +2,67 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+// Stateless Widget
 class MyApp extends StatelessWidget {
-  // このウィジェットはアプリケーションのルートです。
   @override
   Widget build(BuildContext context) {
+    const title = 'Hello Flutter';
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: title,
       theme: ThemeData(
-        // これはアプリケーションのテーマです。
-        //
-        // "flutter run"でアプリケーションを起動してみましょう。青いツールバーを持ったアプリが開きます。
-        // それから、アプリを終了することなく、下記のprimarySwatchをColors.greenに変更してみると、
-        // "hot reload"が呼び出されます（"flutter run"を起動したコンソールで"r"を押すか、
-        // Flutter IDE内の"hot reload"に対して変更を保存するかだけでもOKです）。
-        // カウンターはゼロにリセットされていないということに注目してください。
-        // アプリケーションは再起動されていません。
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.cyan,
       ),
-      home: MyHomePage(title: 'Flutter Sample'),
+      home: HelloWorld(title: title, world: 'world'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // このウィジェットはアプリケーションのホームページです。ステートフルであり、
-  // （以下で定義されている）Stateオブジェクトを持っていることを意味しています。
-  // このStateオブジェクトには、見た目に影響を及ぼすフィールドを含んでいます。
-
-  // このクラスはstateのための設定を行います。親によって提供され（今回のケースではtitle）、
-  // Stateのbuildメソッドによって使われる（今回のケースではApp widget）値を保持します。
-  // Widgetサブクラスのフィールドは常に"final"でマークされます。
+// Stateful Widgate
+class HelloWorld extends StatefulWidget {
+  HelloWorld({Key key, this.title, this.world}) : super(key: key);
   final String title;
+  final String world;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HelloWorld createState() => _HelloWorld();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // このsetStateの呼び出しは、FlutterフレームワークにこのStateの中で何かが変更されたということを伝えます。
-      // これにより、以下のbuildメソッドがreturnされ、更新された値が反映されます。
-      // もし、setState()を呼び出さずに、_counterを変更したとしたら、
-      // buildメソッドは再び呼ばれることはなく、何も起こらないでしょう。
-      _counter++;
-    });
-  }
-
+// State Widget
+class _HelloWorld extends State<HelloWorld> {
   @override
   Widget build(BuildContext context) {
-    // このメソッドはsetStateが呼ばれるたびにreturnされます。
-    // たとえば、上記の_incrementCounterメソッドによってreturnされるように。
-    //
-    // Flutterフレームワークはbuildメソッドが速くreturnするように最適化してきました。
-    // そのためWidgetのインスタンスを個別に変更する必要はなく、更新が必要なものだけを再buildできます。
     return Scaffold(
       appBar: AppBar(
-        // ここではApp.buildメソッドによって作成されたMyHomePageオブジェクトから値を取得し、
-        // appbarのtitleを設定するために使用します。
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Centerはレイアウト用のwidgetです。単一の子を取り、親の中央に配置します。
+          title: Center(
+        child: Text(widget.title),
+      )),
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Columnもまたレイアウト用のwidgetです。子のリストを取り、それらを垂直に並べます。
-          // デフォルトでは、子のサイズに合わせてサイズが変更され、親の高さになります。
-          //
-          // 各ウィジェットのワイヤフレームを表示するために"debug painting"を起動します。
-          //（コンソールで"p"を押すか、
-          // Android StudioであればFlutterインスペクターから"Toggle Debug Paint"アクションを選択するか、
-          // Visual Studio Codeであれば"Toggle Debug Paint"コマンドを実行します。）
-          //
-          // Columnは、自分自身の大きさや子の配置方法を制御するためのさまざまなプロパティを持ちます。
-          // ここでは、子を垂直方向にセンタリングするためにmainAxisAlignmentを使用しています。
-          // ここでの主軸は垂直軸となります。それは列が垂直（交差軸は水平）であるからです。
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Center(
+              child: Text(
+                'Hello',
+                style: TextStyle(
+                  fontSize: 64.0,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Center(
+              child: Text(
+                widget.world,
+                style: TextStyle(
+                  fontSize: 64.0,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // この末尾のカンマは、buildメソッドのための自動フォーマットをより良くします。
     );
   }
 }
